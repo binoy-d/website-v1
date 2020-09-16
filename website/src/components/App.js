2e37a9600c4b13d171bbcb4f92b9a4aea874b5d8
Daniel Binoy <dbinoy15@gmail.com>
2020-09-16 12:30:03 -0700

    getting started

---
+++ website/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>This is a test</h1>
+      <Button variant = "primary">Test button</Button>
+    </>
+  );
+}
+
+export default App;
